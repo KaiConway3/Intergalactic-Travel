a541154acdbef29cff7857cda269117d3fc8690e
Kai Conway <kconway@bsd220.org>
2020-01-09 10:29:15 -0600

    Initial Commit

---
+++ Intergalactic Travel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Intergalactic Travel
+//
+//  Created by Kai Conway on 10/8/19.
+//  Copyright © 2019 Kai Conway. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
